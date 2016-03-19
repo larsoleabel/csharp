1aa489929aab470e91d295e26a05cc7deea0152e
larsoleabel <lars.abel@live.dk>
2016-03-19 17:35:01 +0100

    tutorial
    herfra: https://msdn.microsoft.com/en-us/library/aa664639(v=vs.71).aspx

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[] arr = new int[5];
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = i * i;
+
+            for (int i = 0; i < arr.Length; i++)
+                Console.WriteLine("arr[{0}] = {1}", i, arr[i]);
+
+            string s = "Test";
+            string t = string.Copy(s);
+            Console.WriteLine(s == t);
+            Console.WriteLine((object)s == (object)t);
+            Console.WriteLine(3.ToString());
+            Console.WriteLine("4");
+
+            Console.ReadLine();
+        }
+    }
+}
